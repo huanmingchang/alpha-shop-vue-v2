8ed3dc5928e93e18f4c82b3fe7f56110890190ad
huanmingchang <huanming1988@gmail.com>
2022-02-06 10:38:07 +0800

    feat: add Stepper component and content

---
+++ src/components/Stepper.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="stepper">
+    <div class="stepper__container">
+      <div
+        class="stepper__container__step"
+        v-for="step in steps"
+        :key="step.id"
+        :class="step.status"
+      >
+        <div class="stepper__container__step--circle-container"></div>
+        <div class="stepper__container__step--label-container">
+          {{ step.title }}
+        </div>
+        <span class="stepper__container__step--connect-line"></span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { v4 as uuidv4 } from 'uuid'
+export default {
+  name: 'Stepper',
+  data() {
+    return {
+      steps: [
+        {
+          id: uuidv4(),
+          title: '寄送地址',
+          status: 'initial',
+        },
+        {
+          id: uuidv4(),
+          title: '運送方式',
+          status: '',
+        },
+        {
+          id: uuidv4(),
+          title: '運送方式',
+          status: '',
+        },
+      ],
+    }
+  },
+}
+</script>
---
+++ src/views/Checkout.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="main">
+    <div class="main__title">結帳</div>
+    <Stepper />
+  </main>
+</template>
+
+<script>
+import Stepper from './../components/Stepper'
+
+export default {
+  name: 'Checkout',
+  components: {
+    Stepper,
+  },
+}
+</script>
